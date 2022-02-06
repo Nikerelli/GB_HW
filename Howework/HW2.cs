bd344155e7408c67a968a61e497d3884534b85be
Mikhaik Selyakov <maselyak@stream.ru>
2022-02-06 14:07:23 +0300

    HW2 fixes

--- Howework/HW2.cs
+++ Howework/HW2.cs
@@ -1,88 +1,57 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Howework
 {
     public static class HW2
     {
-        // Task1
-        // https://leetcode.com/problems/jump-game-ii/
-        public static int Jump(int[] nums)
+        // Task10
+        public static void DisplaySecondDigit(int val)
         {
-            int jumps = 0;
-            int longest = -1;
-            int target = 0;
-            for (int i = 0; i < nums.Length - 1; i++)
+            if(val > 999 || val < 100)
             {
-                longest = Math.Max(nums[i] + i, longest);
-                if (i == target)
-                {
-                    jumps++;
-                    target = longest;
-                }
-            }
-            return jumps;
-        }
-
-        // Task2
-        // https://leetcode.com/problems/powx-n/
-        public static double MyPow(double x, int n)
-        {
-            if (n < 0) return 1 / x * MyPow(1 / x, -(n + 1));
-            if (n == 0) return 1;
-            if (n == 1) return x;
-            if (n % 2 == 0) return MyPow(x * x, n / 2);
-            return x * MyPow(x * x, n / 2);
-        }
-
-        // Task3
-        // https://leetcode.com/problems/divide-two-integers/
-        public static int Divide(int dividend, int divisor)
-        {
-            if (divisor == 0 | (dividend == Int32.MinValue && divisor == -1))
-                return Int32.MaxValue;
-
-            bool sign = (dividend > 0) ^ (divisor > 0);
-            uint dd = (uint)(dividend < 0 ? -dividend : dividend);
-            uint dr = (uint)(divisor < 0 ? -divisor : divisor);
-            int reminder = 0;
-
-            for (int i = 31; i >= 0; i--)
-            {
-                if ((dd >> i) >= dr)
-                {
-                    reminder = reminder << 1 | 0x01;
-                    dd -= dr << i;
-                }
-                else
-                    reminder = reminder << 1;
-            }
-            return sign ? -reminder : reminder;
-        }
-
-        // Task4
-        // https://leetcode.com/problems/group-anagrams/
-        public static IList<IList<string>> GroupAnagrams(string[] strs)
-        {
-            var dic = new Dictionary<string, IList<string>>();
-            for(int i = 0; i < strs.Length; i++)
-            {
-                var wordAsArr = strs[i].ToArray();
-                Array.Sort(wordAsArr);
-                var word = new string(wordAsArr);
-
-                if (dic.ContainsKey(word))
-                {
-                    dic[word].Add(strs[i]);
-                }
-                else
-                {
-                    dic.Add(word, new List<string> { strs[i] });
-                }
+                Console.WriteLine("Value should have 3 digits only\n");
+                return;
             }
 
-            return dic.Select(x => x.Value).ToList();
+            Console.WriteLine($"Value: {val}\n2nd digit: {val / 10 % 10}\n");
+        }
+
+        // Task12
+        public static void RemoveSecondDigit(int val)
+        {
+            if (val > 999 || val < 100)
+            {
+                Console.WriteLine("Value should have 3 digits only\n");
+                return;
+            }
+
+            Console.WriteLine($"Value: {val}\nDeleted 2nd digit: {val % 10 + val / 100 * 10}\n");
+        }
+
+        // Task12
+        public static void FindThirdDigit(int val)
+        {
+            if (val < 100)
+            {
+                Console.WriteLine($"Value: {val}\n3d digit does not exist\n");
+                return;
+            }
+
+            Console.WriteLine($"Value: {val}\n3d digit: {val / 100 % 10}\n");
+        }
+
+        public static void DisplayDayOfWeek(int dayOfWeek)
+        {
+            if(dayOfWeek < 0 || dayOfWeek > 6)
+            {
+                Console.WriteLine($"Incorrect input: {dayOfWeek}. It should be in [0,6]\n");
+                return;
+            }
+            var displayPhrase = dayOfWeek == 0 || dayOfWeek == 6
+                ? $"It's weekend day! ({(DayOfWeek)dayOfWeek})\n"
+                : $"It's not weekend day! ({(DayOfWeek)dayOfWeek})\n";
+
+            Console.WriteLine(displayPhrase);
         }
     }
 }
--- Howework/Program.cs
+++ Howework/Program.cs
@@ -6,7 +6,18 @@ namespace Howework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(HW2.GroupAnagrams(new string[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+            HW2.DisplaySecondDigit(123);
+            HW2.RemoveSecondDigit(123);
+            HW2.RemoveSecondDigit(10);
+            HW2.RemoveSecondDigit(1000);
+            HW2.FindThirdDigit(1234);
+            HW2.FindThirdDigit(12);
+            HW2.FindThirdDigit(123);
+            HW2.DisplayDayOfWeek(-1);
+            HW2.DisplayDayOfWeek(8);
+            HW2.DisplayDayOfWeek(6);
+            HW2.DisplayDayOfWeek(0);
+            HW2.DisplayDayOfWeek(3);
         }
 
 
