b9511a4c030a87aead7be31a6eea85850755f19a
Mikhaik Selyakov <maselyak@stream.ru>
2022-03-03 12:00:34 +0300

    HW 9

---
+++ Howework/HW9.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Howework
+{
+    public static class HW9
+    {
+        public static void Task64(int m, int n)
+        {
+            while (m <= n)
+            {
+                Console.WriteLine(m++);
+            }
+        }
+        
+        public static void Task66(int m, int n)
+        {
+            var sum = 0;
+            while (m <= n)
+            {
+                sum += m++;
+            }
+            
+            Console.WriteLine($"Sum: {sum}");
+        }
+        
+        public static int Task68(int m, int n, int level = 1)
+        {
+            Console.WriteLine($"Level #{level}, m = {m}, n = {n}");
+            if (m == 0)
+            {
+                return n + 1;
+            }
+
+            if (m > 0 && n == 0)
+            {
+                return Task68(m - 1, 1, level + 1);
+            }
+
+            return Task68(m - 1, Task68(m, n - 1, level + 2), level + 1);
+        }
+    }
+}
--- Howework/Program.cs
+++ Howework/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Howework
 {
@@ -6,19 +7,12 @@ namespace Howework
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"\n{nameof(HW8.Task57)}");
-            int[][] arr = new int[8][];
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = new int[10].FillGeneratedValues();
-            }
-            HW8.Task57(arr);
-            Console.WriteLine($"\n{nameof(HW8.Task59)}");
-            HW8.Task59(arr);   
-            Console.WriteLine($"\n{nameof(HW8.Task61)}");
-            HW8.Task61();
-            Console.WriteLine($"\n{nameof(HW8.Task63)}");
-            HW8.Task63();
+            Console.WriteLine($"\n{nameof(HW9.Task64)}");
+            HW9.Task64(1,4);
+            Console.WriteLine($"\n{nameof(HW9.Task66)}");
+            HW9.Task66(1,15);
+            Console.WriteLine($"\n{nameof(HW9.Task68)}");
+            HW9.Task68(1,5);
         }
     }
 }
